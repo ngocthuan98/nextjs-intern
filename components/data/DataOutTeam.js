91ccea995fddd141decea0aad18a42b091ff73c2
unknown <ngocthuandn98@gmail.com>
2023-05-23 16:26:15 +0700

    upload img

--- components/data/DataOutTeam.js
+++ components/data/DataOutTeam.js
@@ -1,6 +1,6 @@
 const arrayImgTeam = [
-    { id: 1, name: "Michael Scott", image: "/image/img10.png" },
-    { id: 2, name: "Dwight Schrute", image: "/image/img9.png" },
+    { id: 1, name: "Michael Scott", image: "https://images.unsplash.com/photo-1517865288-978fcb780652?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=465&q=80" },
+    { id: 2, name: "Dwight Schrute", image: "/image/img10.png" },
     { id: 3, name: "Pam Beetsley", image: "/image/img11.png" },
   ];
   
